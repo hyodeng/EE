f85afe476703e4d4ed14555b403ac5baa30634f0
ShinJH <ShinJH@users.noreply.github.com>
2022-08-20 00:09:45 +0900

    신종혁 - 전투 씬 구현 가이드라인

---
+++ Assets/Scripts/Battle/Battle.cs
@@ -0,0 +1,103 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Battle : MonoBehaviour
+{
+    public Player[] player;
+    public Monster[] monster;
+    public CharacterData[] characters;
+
+    public List<CharacterData> charactersList;
+    public List<SpriteRenderer> parts;
+
+    public Camera cam;
+
+    public GameObject[] Avatars;
+    public Transform Operator;
+    public bool Focusing;
+
+    public int index = 0;
+
+    public GameObject TargetPoint;
+
+    private void Start()
+    {
+        Initialize();
+    }
+    private void Update()
+    {
+        if (Focusing)
+        {
+            cam.orthographicSize = 2f;
+            cam.transform.position = Avatars[index].transform.position - new Vector3(0, 0, 10);
+        }
+        else
+        {
+            cam.orthographicSize = 5f;
+            cam.transform.position = Vector3.zero - new Vector3(0, 0, 10);
+        }
+        if(index == 8)
+        {
+            index = 0;
+        }
+    }
+    private void Initialize()
+    {
+        TargetPoint.SetActive(false);
+        player = Transform.FindObjectsOfType<Player>();
+        System.Array.Sort<Player>(player, (x, y) => x.transform.GetSiblingIndex().CompareTo(y.transform.GetSiblingIndex()));
+        monster = Transform.FindObjectsOfType<Monster>();
+        System.Array.Sort<Monster>(monster, (x, y) => x.transform.GetSiblingIndex().CompareTo(y.transform.GetSiblingIndex()));
+
+        Operator.GetChild(0).GetComponent<Button>().onClick.AddListener(() => OnTarget());
+        Operator.GetChild(1).GetComponent<Button>().onClick.AddListener(() => OperateCharacter(2));
+        Operator.GetChild(2).GetComponent<Button>().onClick.AddListener(() => OperateCharacter(3));
+
+        SetTurn();
+    }
+    public void SetTurn()
+    {
+        characters = FindObjectsOfType<CharacterData>();
+        charactersList = new List<CharacterData>(characters);
+
+        for (int i = 0; i < characters.Length; i++)
+        {
+            for (int j = i + 1; j < characters.Length; j++)
+            {
+                if (characters[i].speed < characters[j].speed)
+                {
+                    (characters[i], characters[j]) = (characters[j], characters[i]);
+                }
+            }
+        }
+        for (int i = 0; i < charactersList.Count; i++)
+        {
+            charactersList.Sort((x, y) => x.speed.CompareTo(y.speed) * (-1));
+        }
+        System.Array.Resize(ref Avatars, characters.Length);
+        for (int i = 0; i < characters.Length; i++)
+        {
+            Avatars[i] = characters[i].gameObject;
+        }
+    }
+    public void OperateCharacter(int State)
+    {
+        characters[index].State = State;
+        Operator.gameObject.SetActive(false);
+    }
+    public void CameraMove()
+    {
+
+    }
+    public void OnTarget()
+    {
+        TargetPoint.SetActive(true);
+        Targetting(0);
+    }
+    public void Targetting(int target)
+    {
+        TargetPoint.transform.position = new Vector3(6, -98.5f + target * 2f , 0);
+    }
+}
--- Assets/Scripts/CharacterData/CharacterData.cs
+++ Assets/Scripts/CharacterData/CharacterData.cs
@@ -1,12 +1,92 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class CharacterData : MonoBehaviour
 {
-    public character character;
-
-    private void Start()
+    public Vector3 oriPos;
+    public float moveSpeed = 5f;
+    public Animator anim;
+    public Battle battle;
+    public int state = 0; //0 : 대기 , 1 : 공격, 2 : 스킬, 3 : 아이템, 4 : 사망
+    public int hp, mp, attack, magic, defence, speed;
+    public bool BehaviorEnd;
+    public int State
     {
+        get => state;
+        set
+        {
+            state = value;
+            switch (value)
+            {
+                case 0:
+                    anim.Play("0_Idle");
+                    break;
+                case 1:
+                    StartCoroutine(Move());
+                    anim.Play("1_Run");
+                    break;
+            }
+        }
+    }
+    IEnumerator Move()
+    {
+        while(transform.position != Vector3.zero)
+        {
+            transform.position  = Vector3.MoveTowards(transform.position, Vector3.zero, Time.deltaTime * moveSpeed);
+            yield return null;
+        }
+        yield return StartCoroutine(Attack());
+    }
+    private void Awake()
+    {
+        anim = GetComponent<Animator>();
+        oriPos = transform.position;
+    }
+    public void StateChange(int i)
+    {
+        state = i;
+    }
+    public void Idle()
+    {
+        battle.Operator.gameObject.SetActive(false);
+    }
+    public IEnumerator Attack()
+    {
+        anim.Play("2_Attack_Normal");
+        yield return new WaitForSeconds(1f);
+        while (!BehaviorEnd)
+        {
+            yield return null;
+        }
+        BehaviorEnd = false;
+        transform.localScale = new Vector3(-2, 2, 2);
+        anim.Play("1_Run");
+        while (transform.position != oriPos)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, oriPos, Time.deltaTime * moveSpeed);
+            yield return null;
+        }
+        transform.localScale = new Vector3(2, 2, 2);
+        State = 0;
+        battle.Operator.gameObject.SetActive(true);
+        battle.index++;
+    }
+    public void AttackEnd()
+    {
+        BehaviorEnd = true;
+    }
+    public void OnMouseEnter()
+    {
+        battle.Targetting(Convert.ToInt32(gameObject.name[gameObject.name.Length - 1]));
+    }
+    public void OnMouseOver()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            battle.OperateCharacter(1);
+            battle.TargetPoint.SetActive(false);
+        }
     }
 }
