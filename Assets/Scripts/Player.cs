15a763b608be15785d0198e9427003160d28ee9a
GoatKINGDONG <105641714+GoatKINGDONG@users.noreply.github.com>
2022-09-01 19:45:30 +0900

    캐릭터 이동

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,44 +6,68 @@ using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
-    public float runSpeed = 6.0f;
-    Animator anim;
-    
     PlayerInputActions actions;
+    public float MoveSpeed = 3.0f;
     Rigidbody rigid;
-
+    Animator anim;
     Vector3 inputDir = Vector3.zero;
     
 
     private void Awake()
     {
-       
-        anim = GetComponent<Animator>();
-        actions = new PlayerInputActions();
         rigid = GetComponent<Rigidbody>();
+        actions = new PlayerInputActions();
+        anim = GetComponent<Animator>();
     }
     private void OnEnable()
     {
-        
         actions.Player.Enable();
-        actions.Player.Move.performed += OnMoveInput;
-        actions.Player.Move.canceled += OnMoveInput;
-    }
-    private void OnDisable()
-    {
-        actions.Player.Move.canceled -= OnMoveInput;
-        actions.Player.Move.performed -= OnMoveInput;
-        actions.Player.Disable();
-    }
-    private void FixedUpdate()
-    {
-        rigid.MovePosition(rigid.position + transform.forward * inputDir.y * runSpeed * Time.fixedDeltaTime);
-    }
-    void OnMoveInput(InputAction.CallbackContext context)
-    {
-        inputDir = context.ReadValue<Vector2>();
+        actions.Player.Move.performed += OnMove;
+        actions.Player.Move.canceled += OnStop;
+        actions.Player.Use.performed += OnUse;
         
     }
 
-    
+    private void OnDisable()
+    {
+        
+        actions.Player.Use.performed -= OnUse;
+        actions.Player.Move.canceled -= OnStop;
+        actions.Player.Move.performed -= OnMove;
+        actions.Player.Disable();
+    }
+    void OnMove(InputAction.CallbackContext context)
+    {
+        Vector2 input = context.ReadValue<Vector2>();
+
+        inputDir.x = input.x;
+        inputDir.y = 0.0f;
+        inputDir.z = input.y;
+        inputDir.Normalize();
+
+        anim.SetBool("isMove", true);
+        anim.SetFloat("inputX", inputDir.x);
+        anim.SetFloat("inputY", inputDir.y);
+    }
+    void OnStop(InputAction.CallbackContext context)
+    {
+        Vector2 input = context.ReadValue<Vector2>();
+
+        inputDir.x = input.x;
+        inputDir.y = 0.0f;
+        inputDir.z = input.y;
+        inputDir.Normalize();
+        anim.SetBool("isMove", false);
+    }
+
+    void OnUse(InputAction.CallbackContext context)
+    {
+        anim.SetBool("Use", true);
+    }
+    private void FixedUpdate()
+    {
+        rigid.MovePosition(rigid.position + MoveSpeed * Time.fixedDeltaTime * inputDir);
+    }
+
+
 }
