2307065a0ebfe2e10a17cbe33fb68b2e57fdea0b
KilJongHwan <105642014+KilJongHwan@users.noreply.github.com>
2022-09-04 13:57:11 +0900

    Enum 추가

--- Assets/Scripts/CharacterData/CharaterType.cs
+++ Assets/Scripts/CharacterData/CharaterType.cs
@@ -4,7 +4,13 @@ using UnityEngine;
 
     public enum CharacterType
     {
-        warrior = 0, mage, cleric, thief, popstar, chef
+        Warrior = 0, Mage, Cleric, Thief, Popstar, Chef
     }
+    public enum ItemCode
+    {
+        Weapon = 0, Armor, Consumable
+    }
+
+
 
    // int count = System.Enum.GetValues(typeof(CharacterType)).Length;
